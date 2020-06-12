ac0dbdd85e8f90d64211d61cb2885edd8be8d6e8
ABC <ABC@abc.com>
2020-06-12 10:38:50 +0800

    Initial Commit

---
+++ YellowShopRating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YellowShopRating
+//
+//  Created by Kwok Wai Yeung on 12/6/2020.
+//  Copyright © 2020 Kwok Wai Yeung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
